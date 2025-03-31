fcdb4c0b697abbefe3e4821e3c1ef7215e7247f9
klargdev <klargdev+github@gmail.com>
2025-03-31 01:18:20 +0000

    update app.jsx

---
+++ evera-website/src/App.jsx
@@ -0,0 +1,29 @@
+// src/App.jsx
+import { Routes, Route } from 'react-router-dom'
+import { AnimatePresence } from 'framer-motion'
+import Layout from './components/layout/Layout'
+import Home from './pages/Home'
+import Services from './pages/Services'
+import Portfolio from './pages/Portfolio'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Blog from './pages/Blog'
+
+function App() {
+  return (
+    <Layout>
+      <AnimatePresence mode="wait">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/blog" element={<Blog />} />
+        </Routes>
+      </AnimatePresence>
+    </Layout>
+  )
+}
+
+export default App
